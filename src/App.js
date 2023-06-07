e2471934125cc9810c0a32ddc10105d77d0c9fb4
johnny-gill <uint777@gmail.com>
2023-06-07 23:56:32 +0900

    ㅇ리단

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+
+const heavyWork = () => {
+  console.log('엄청 무거운 작업');
+  return ['test'];
+};
+
 function App() {
+  const [names, setNames] = useState(heavyWork);
+  const [input, setInput] = useState('');
+
+  const inputChangeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const uploadClickHandler = () => {
+    setNames([input, ...names]);
+  };
+
   return (
     <div>
-      <span>bb</span>
+      <input type="text" onChange={inputChangeHandler} />
+      <button onClick={uploadClickHandler}>UPLOAD</button>
+      {names.map((name, idx) => (
+        <p key={idx}>{name}</p>
+      ))}
     </div>
   );
 }
